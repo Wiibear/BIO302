1afb3b6299a3e5986e13f477447e559e7b052f4e
Vebjørn <kvebops@gmail.com>
2022-06-02 14:42:51 +0200

    Added penguin discrimination

--- Penglings.R
+++ Penglings.R
@@ -3,3 +3,8 @@ library(tidyverse)
 
 ggplot(data = penguins, aes(bill_length_mm, flipper_length_mm))+
   geom_point()
+
+create_from_github("Wiibear/BIO302-electric-bogaloo")
+
+discriminating_penguins <- penguins |> 
+  filter(species != "Adelie")
